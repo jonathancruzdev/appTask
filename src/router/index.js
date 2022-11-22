a4d8820400935b73e230128927290dab2ad42638
Jonathan Cruz <cruzjonathan.e@gmail.com>
2022-11-22 10:24:53 -0300

    Rutas

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import CompletadasView from '../views/CompletadasView'
+import PendientesView from '../views/PendientesView'
+import NuevaView from '../views/NuevaView'
 
 Vue.use(VueRouter)
 
@@ -9,6 +12,21 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/completadas',
+    name: 'completadas',
+    component: CompletadasView
+  },
+  {
+    path: '/pendientes',
+    name: 'pendientes',
+    component: PendientesView
+  },
+  {
+    path: '/nueva',
+    name: 'nueva',
+    component: NuevaView
   },
   {
     path: '/about',
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container>
+    <div class="about">
+      <h1>Nuestra app</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis corporis doloribus exercitationem quisquam aperiam! Unde iste exercitationem repellendus autem, numquam ducimus quos explicabo officiis ut culpa perferendis at eius optio?</p>
+    </div>
+  </v-container>
+
 </template>
